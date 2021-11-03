c5c7fd7f76bbaf2e356d901d0553888b113c8071
Mohamed Shahul Hameed <thameemsh@email.com>
2021-11-03 19:54:53 -0400

    Add listener

--- client.js
+++ client.js
@@ -10,20 +10,36 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  conn.on('data', (data) => {
-    console.log('Server: ', data);
-  })
 
   conn.on('connect', () => {
-  console.log("Successfully connected to game server")
-   conn.write("Name: MTA");
-   conn.write("Move: up")
+   console.log("Successfully connected to game server")
+   conn.write('Name: MTA');
   });
 
-  return conn;
+  // To move the snake around.
+
+    conn.on('connect', () => {
+    // conn.write('Move: up');
+    let b = 0;
+    
+    for (let i = 0; i < 20; i++) {
+      setTimeout(()=> conn.write("Move: right"), 50 * i);
+      b = 50 * i;
+    }
+   
+    for (let i = 0; i < 20; i++) {
+      setTimeout(()=> conn.write("Move: up"), b + 50 * i);
+      b = b + 50 * i;
+    }
+   });
+ 
+
+  conn.on('data', (data) => {
+    console.log('Server: ', data);
+  });
+
+  return conn ;
 };
 
 
-
-
-module.exports= connect;
+module.exports= {connect};
--- play.js
+++ play.js
@@ -1,10 +1,22 @@
 const net = require("net");
-const connect = require("./client")
-
-
-
+const {connect} = require("./client")
 
 console.log("Connecting ...");
 connect();
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+  return stdin;
+};
 
