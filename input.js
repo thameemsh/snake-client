789fce5a6bf8c781139a0d06ed62c13f4dee1f51
Mohamed Shahul Hameed <thameemsh@email.com>
2021-11-03 20:08:40 -0400

    Add listener

---
+++ input.js
@@ -0,0 +1,20 @@
+const { METHODS } = require("http");
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function (data) {
+  if (data === '\u0003') {
+    process.exit();
+  }
+};
+
+module.exports = {setupInput}
--- play.js
+++ play.js
@@ -1,22 +1,7 @@
-const net = require("net");
-const {connect} = require("./client")
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
-  return stdin;
-};
-
+setupInput();
