3b09dac0d9f37cd92ad6e2d21b96450597669589
Mohamed Shahul Hameed <thameemsh@email.com>
2021-11-03 21:03:44 -0400

    Add key to play game and send messages

--- client.js
+++ client.js
@@ -14,24 +14,25 @@ const connect = function () {
   conn.on('connect', () => {
    console.log("Successfully connected to game server")
    conn.write('Name: MTA');
+   // conn.write('Move: up');
   });
 
   // To move the snake around.
 
-    conn.on('connect', () => {
-    // conn.write('Move: up');
-    let b = 0;
+  //   conn.on('connect', () => {
+  //   
+  //   let b = 0;
     
-    for (let i = 0; i < 20; i++) {
-      setTimeout(()=> conn.write("Move: right"), 50 * i);
-      b = 50 * i;
-    }
+  //   for (let i = 0; i < 20; i++) {
+  //     setTimeout(()=> conn.write("Move: right"), 50 * i);
+  //     b = 50 * i;
+  //   }
    
-    for (let i = 0; i < 20; i++) {
-      setTimeout(()=> conn.write("Move: up"), b + 50 * i);
-      b = b + 50 * i;
-    }
-   });
+  //   for (let i = 0; i < 20; i++) {
+  //     setTimeout(()=> conn.write("Move: up"), b + 50 * i);
+  //     b = b + 50 * i;
+  //   }
+  //  });
  
 
   conn.on('data', (data) => {
--- input.js
+++ input.js
@@ -1,6 +1,8 @@
-const { METHODS } = require("http");
+// Stores the active TCP connection object.
+let connection;
 
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,8 +13,23 @@ const setupInput = function () {
   return stdin;
 };
 
-const handleUserInput = function (data) {
-  if (data === '\u0003') {
+const handleUserInput = function (key) {
+
+  connection.write('Say: I am flying');
+  
+  if (key == "w") {
+   connection.write('Move: up');
+  }
+  if (key == "a") {
+    connection.write('Move: left');
+   }
+   if (key == "d") {
+    connection.write('Move: right');
+   }
+   if (key == "s") {
+    connection.write('Move: down');
+   }
+  if (key == '\u0003') {
     process.exit();
   }
 };
